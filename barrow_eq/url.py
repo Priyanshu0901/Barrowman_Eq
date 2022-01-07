6711f10dd1f5e549e286df1942fe58cde75e2418
Priyanshu0901 <52269169+Priyanshu0901@users.noreply.github.com>
2022-01-07 12:06:32 +0530

    Add files via upload

--- barrow_eq/url.py
+++ barrow_eq/url.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from . import views
 
@@ -7,3 +8,5 @@ urlpatterns = [
     path('',views.home,name='barrowman_equations'),
     path('eq',views.eq,name='eq result')
 ]
+
+urlpatterns += staticfiles_urlpatterns()
